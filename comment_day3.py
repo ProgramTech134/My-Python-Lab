a7f09b9429f69ce0914ecb4969d7629cad98e455
Program Tech <techprogram9090@gmail.com>
2026-04-19 11:29:26 -0700

    Add files via upload
    Title: Day 3: Python Basics - Escape Sequences & Comments
    
    Description:
    "Today is the 3rd day of my Python learning journey! In this session, I practiced:
    
    Escape Sequences: Using \n for new lines and \" for quotes inside strings.
    
    Comments: Practiced both single-line (#) and multi-line comments (''' or """).
    
    Print Function: Exploring sep (separator) and end parameters to format output.
    
    I'm building my foundation in programming step-by-step. If you find this helpful, please drop a ⭐ to support my journey!"

---
+++ comment_day3.py
@@ -0,0 +1,23 @@
+#If you have find my python file then please give me one star.I am sure your one star make my day today 
+print ("hello guys today is my 3rd day in \"programming language\" and \n i am learning comments how to use comment in python \nand what is escape sequence character.")
+# Now i am using multiple comment
+#There are two different method for multiple comment you can see below 
+"""
+This is 
+first method 
+to write 
+multiple comment
+"""
+
+'''
+and this 
+is second 
+method to write 
+multiple commnent
+
+'''
+print("programming", 3, 4 , 8 , sep=" ~ " ,end="009\n")
+print("harry")
+
+# I hope this make your python practice day easy.
+
