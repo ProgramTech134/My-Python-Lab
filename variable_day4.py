b151779f157e1bf7115ab5e6927585618d5c838e
Program Tech <techprogram9090@gmail.com>
2026-04-20 10:55:41 -0700

    Add files via upload
    Title: Day 1: Understanding Variables and Data Types in Python
    
    Description:
    Today, I started my Python learning journey by exploring how variables work and how Python identifies different data types.
    
    In this script, I have:
    
    Initialized variables with different types: Integer, String, NoneType, and Boolean.
    
    Used the type() function to inspect and confirm the data type of each variable.
    
    Performed basic arithmetic by adding two integer variables.   
    Code Snippet Overview:
    
    a = 6 (Integer)
    
    b = "gwangum" (String)
    
    c = None (NoneType)
    
    d = True (Boolean)
    
    Result of e + f demonstrates basic math operations in Python.
    
    #PythonLearning #CodingNewbie  #PythonVariables

---
+++ variable_day4.py
@@ -0,0 +1,15 @@
+                          #Today i am learning variables in python
+a = 6 
+b = "gwangum"
+c =  None 
+d =  True 
+
+print("what is the type of a " , type(a))
+print("what is the type of b " , type(b))
+print("what is the type of c " , type(c))
+print("what is the type of d " , type(d))
+
+e = 10 
+f = 11 
+print(e + f)
+
