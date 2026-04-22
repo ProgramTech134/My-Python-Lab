692542a6ad864c9971e2d9b2c267b3671cb80b11
Program Tech <techprogram9090@gmail.com>
2026-04-22 04:00:13 -0700

    Add files via upload
    Basic Arithmetic Operations in Python
    This is a simple Python script created to demonstrate the use of various Arithmetic Operators. It is part of my journey to learn Python programming and covers the fundamental mathematical calculations used in coding. 
    What’s Included:
    The script performs the following operations:
    
    Addition (+): Adding two numbers.
    
    Subtraction (-): Subtracting one number from another.
    
    Multiplication (*): Multiplying numbers.
    
    Division (/): Standard division (returns a float).
    
    Modulus (%): Finding the remainder of a division.
    
    Floor Division (//): Dividing and rounding down to the nearest whole number.
    
    Exponentiation (**): Calculating the power of a number.

---
+++ Day6_Arithmatics.py
@@ -0,0 +1,23 @@
+n = 8
+m = 20
+
+ans1 = n + m 
+print("The sum of m and n is :", ans1)
+
+ans2 = n - m 
+print("The sum of m and n is :", ans2)
+
+ans3 = n * m 
+print("The sum of m and n is :", ans3)
+
+ans4 = n / m 
+print("The sum of m and n is :", ans4)
+
+ans5 = n % m 
+print("The sum of m and n is :", ans5)
+
+ans6 = n // m 
+print("The sum of m and n is :", ans6)
+
+ans7 = n**m 
+print("The sum of m and n is :", ans7)
