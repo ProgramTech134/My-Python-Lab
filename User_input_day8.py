70034f0916f310056dfcb38a8ef0fed3ebe94a52
Program Tech <techprogram9090@gmail.com>
2026-04-24 04:48:19 -0700

    Add files via upload
    📝 Python Input & Typecasting
    Is program mein maine seekha hai ke user se input kaise lete hain aur Typecasting kyun zaroori hai.
    
    Key Learnings:
    Default Input: Python ka input() function hamesha data ko String ki surat mein leta hai.
    
    Typecasting: Agar hum do numbers ko jama (add) karna chahte hain, toh humein int() use karke unhein integer mein tabdeel karna parta hai.
    
    Logic: Agar hum typecasting nahi karenge, toh Python numbers ko sirf aapas mein jorr dega (maslan "5" + "5" = "55").

---
+++ User_input_day8.py
@@ -0,0 +1,14 @@
+a = input("Enter the name :")
+
+print("My name is : " , a)  
+
+# Note: The input() function always returns data as a 'string'.
+# To perform addition, we must convert these strings into integers using Typecasting.
+
+b = input("Enter the first number :")
+c = input("Enter the second number :")
+
+# Converting strings to integers and calculating sum
+
+print( int(b)  + int(c))
+
