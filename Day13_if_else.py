ce34242de2b4aaf04ebb4eb4e0cdccb9833c1b10
Program Tech <techprogram9090@gmail.com>
2026-07-03 15:40:01 +0500

    Add files via upload
    The Problem: Airport Luggage Checker
    At an airport, the maximum allowed weight for a bag is 23 kg.
    
    Write a Python program that takes the weight of a bag as input from the user:
    
    If the weight is 23 kg or less, print: "Luggage cleared! No extra charges."
    
    Else (if the weight is more than 23 kg), print: "Overweight! Extra charges applied."
    
    Note: Use only if and else in your code.

---
+++ Day13_if_else.py
@@ -0,0 +1,10 @@
+weight_of_Luggage = int(input("Enter the weight of Luggage :"))
+
+
+print("your Luggage weight is :",weight_of_Luggage )
+
+if(weight_of_Luggage<=23):
+    print("Luggage cleared! No extra charges.")
+
+else :
+    print("Overweight! Extra charges applied.")
