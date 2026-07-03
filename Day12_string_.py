19377a6c3ab07154f72832c5256d09cb7b446c69
Program Tech <techprogram9090@gmail.com>
2026-07-03 14:48:33 +0500

    Add files via upload

---
+++ Day12_string_.py
@@ -0,0 +1,28 @@
+apple = '''hi
+my name 
+is 
+fatima 
+i am 21
+years old 
+my hobby is sketching
+'''
+print(apple)
+
+name = "song jong ki "
+print( name[0])
+print( name[1])
+print( name[2])
+print( name[3])
+print( name[4])
+print( name[5])
+print( name[6])
+print( name[7])
+print( name[8])
+print( name[9])
+print(name[10])
+print(name[11])
+
+
+print("lets use for loop")
+for character in name:
+ print(character)
