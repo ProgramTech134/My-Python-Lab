a80ce0cb4f939622246cd6a283f7fd54a04a7263
Program Tech <techprogram9090@gmail.com>
2026-07-04 17:54:44 +0500

    Add files via upload
    Your Task: The Traffic Light Challenge
    Write a program that checks the color of a traffic light and tells the driver what to do:
    
    If the color is "red", print "Stop!"
    
    If the color is "yellow", print "Slow down!"
    
    If the color is "green", print "Go!"
    
    If the color is anything else, print "Invalid color!"
    
    (Hint: Create a variable named light_color = "red" at the top to test your code.)

---
+++ Day13-if_elif_else_statement.py
@@ -0,0 +1,30 @@
+# Your Task: The Traffic Light Challenge
+# Write a program that checks the color of a traffic light and tells the driver what to do:
+
+# If the color is "red", print "Stop!"
+
+# If the color is "yellow", print "Slow down!"
+
+# If the color is "green", print "Go!"
+
+# If the color is anything else, print "Invalid color!"
+
+# (Hint: Create a variable named light_color = "red" at the top to test your code.)
+
+Light_colour = str(input("Enter the light colour")).lower().strip()
+
+if(Light_colour == "red") :
+    print("stop")
+
+elif(Light_colour== "yellow"):
+    print("slow down")
+
+elif(Light_colour=="green"):
+    print("Go")
+else :
+    print("invalid colour") 
+
+                     #_____________(special note)_____________ 
+    #if you have any query about my program then you can ask me in comment 
+    #And you find any mistake in my program then tell me in comment it is very helpfull  for me.
+
